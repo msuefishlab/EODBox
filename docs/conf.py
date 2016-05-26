326d916f28a2a98a72dbf40f0b32293e15e1ed77
jasongallant <jgallant@msu.edu>
2016-05-26 11:51:41 -0400

    Added CC License

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'EODBox'
-copyright = u'2016, Bailey Winter, Jason Gallant'
+copyright = u'2016, Bailey Winter and Jason Gallant.  Licensed under Creative Commons CC BY-NC-SA.'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
